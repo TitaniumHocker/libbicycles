411fe4f7d68316f0b03814a430efef87847e9bb4
Ivan Fedorov <inbox@titaniumhocker.ru>
2021-10-01 11:25:46 +0300

    Fixed djb2 warning

--- src/hash.c
+++ src/hash.c
@@ -5,7 +5,7 @@ extern unsigned long djb2(char* string) {
   unsigned long hash = BICYCLES_DJB2_MAGIC_HASH;
   int c;
 
-  while (c = *string++)
+  while ((c = *string++))
     hash = ((hash << 5) + hash) + c;
   
   return hash;
