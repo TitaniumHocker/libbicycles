ea3d7394eeafdaa7b4fe5d1eccfa5be6ec12e8e1
Ivan Fedorov <inbox@titaniumhocker.ru>
2021-10-02 22:17:18 +0300

    Updated hash header

--- src/hash.h
+++ src/hash.h
@@ -1,20 +1,30 @@
+/**
+ * @file hash.h
+ * @author Ivan Fedorov <inbox@titaniumhocker.ru>
+ * @copyright MIT Licence
+ * @date 2021
+ * @brief File containing implementation of some hash functions.
+ * 
+ * @see https://en.wikipedia.org/wiki/Hash_function
+ * 
+ */
+
 #ifndef __LIBBICYCLES_HASH_H__
 #define __LIBBICYCLES_HASH_H__
 
 #define BICYCLES_DJB2_MAGIC_HASH 5381 ///< Magic start hash for djb2 hash function
 
 /**
- * djb2 hash function
+ * @brief djb2 hash function
  * 
  * Written by Daniel J. Bernstein (also known as djb),
  * this simple hash function dates back to 1991.
  * 
  * This hash function is similar to a Linear Congruential Generator.
  * LCG - a simple class of functions that generate a series of psuedo-random numbers.
- * More info about LCG can be found here: <https://en.wikipedia.org/wiki/Linear_congruential_generator>.
  * 
- * Also see small article about this hash function:
- * <https://theartincode.stanis.me/008-djb2/>
+ * @see <https://theartincode.stanis.me/008-djb2/>
+ * @see <https://en.wikipedia.org/wiki/Linear_congruential_generator>.
  * 
  * @param string String for hashing.
  * @returns Result number of srting hashing.
